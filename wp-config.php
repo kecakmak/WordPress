cae44e40077257c7e8349681b82f26b8cb2dd719
GTPE Student <lab-user@studentvm.2zbvv.internal>
2021-06-17 07:57:15 +0000

    wp settings

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv('DB_NAME') );
+define( 'DB_NAME', 'wordpressd' );
 
 /** MySQL database username */
-define( 'DB_USER', getenv('DB_USER') );
+define( 'DB_USER', 'wordpressu' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
+define( 'DB_PASSWORD', 'wordpressp' );
 
 /** MySQL hostname */
-define( 'DB_HOST', getenv('DB_HOST') );
+define( 'DB_HOST', 'mysql-55-rhel7' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
